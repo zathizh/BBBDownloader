a44d0417571dafe8da3cef8e3fcedb307738eea6
zathizh <sathish.bowatta@gmail.com>
2021-01-16 12:48:53 -0500

    adding ffmpeg for windows

--- BBBDownlaoder.py
+++ BBBDownlaoder.py
@@ -3,52 +3,85 @@
 import requests
 import re
 import argparse
-import os.path
+import os
 import sys
-import time
 
 # VERSION : 1.0.0.1
 # AUTHOR  : Sathish Bowatta
 
-# --- GLOBALS ---
-_CHUNK_SIZE=1024
+# --- OS PLATFORM ---
+def get_platform():
+    return sys.platform.lower()
 
 # --- PATH SEPERATOR ---
 def get_separator():
-    platform = sys.platform.lower()
-
-    if platform == "linux" or "darwin":
+    platform = get_platform()
+    sep = ""
+    if platform == "linux" or "darwin" :
         sep = "/"
-    elif platform == "windows":
+    elif platform == "win32" or platform == "windows" :
         sep = "\\"
     else :
         exit("[-] Invalid Platform")
     return str(sep)
 
-# --- DOWNLOAD WEBM ---
-def video_downloader(_url, _id, _dest):
-    global CHUNK_SIZE
-    file_name = _dest + get_separator() + _id  + ".webm"
+# --- DOWNLOAD WEBM FILES ---
+def downloader(_url, path, name, _id):
+    file_name =  path + get_separator() + name
 
-    with requests.get(_url, stream=True) as response:
+    print("[+] Downloading : ", end="")
+    print(_url)
+
+    with requests.get(_url) as response:
         with open(file_name, 'wb') as f:
-            for chunk in response.iter_content(chunk_size=_CHUNK_SIZE):
-                f.write(response.content)
-# --- MAIN ---
+            f.write(response.content)
+
+# --- CREATE OUTPUT DIRECTORY ---
+def create_directory(path):
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+# --- MP4 PROCESSOR ---
+def mp4_processor(path, _id):
+    platform = get_platform()
+
+    if platform == "linux":
+        fpath = path + get_separator()
+        gen_audio_cmd = "ffmpeg -i " + fpath + "webcams.webm -vn -acodec copy " + fpath + "audio_out.opus"
+        prep_mp4_cmd = "ffmpeg -i " + fpath + "deskshare.webm -i " + fpath + "audio_out.opus -map 0:v -map 1:a -c copy -y " + fpath + _id + ".webm"
+        os.system(gen_audio_cmd)
+        os.system(prep_mp4_cmd)
+
+    elif platform == "win32" or platform == "windows" :
+        fpath = path + get_separator()
+        gen_audio_cmd = ".\\bin\\ffmpeg.exe -i " + fpath + "webcams.webm -vn -acodec copy " + fpath + "audio_out.opus"
+        prep_mp4_cmd = ".\\bin\\ffmpeg.exe -i " + fpath + "deskshare.webm -i " + fpath + "audio_out.opus -map 0:v -map 1:a -c copy -y " + fpath + _id + ".webm"
+        os.system(gen_audio_cmd)
+        os.system(prep_mp4_cmd)
+
+    else :
+        exit("[-] Invalid Platform, Unable to convert files !!!")
+
+
+# --- MAIN FUNCTION ---
 def main():
     parser = argparse.ArgumentParser(prog="BBBVideoDownloader")
     parser.add_argument("-u", "--url", dest="url", action="store", type=str, required=True, help="Meeting URL")
-    parser.add_argument("-d", "--dest", dest="dest", action="store", type=str, required=True, help="Download Directory")
 
     args = parser.parse_args()
     url  = args.url
-    dest = args.dest
 
-    meeting_url = str(re.sub("playback\/[a-zA-Z0-9\.\/]*\?meetingId=", "presentation/", url)) + "/deskshare/deskshare.webm"
+    meeting_deskshare_url = str(re.sub("playback\/[a-zA-Z0-9\.\/]*\?meetingId=", "presentation/", url)) + "/deskshare/deskshare.webm"
+    meeting_webcam_url = str(re.sub("playback\/[a-zA-Z0-9\.\/]*\?meetingId=", "presentation/", url)) + "/video/webcams.webm"
     meeting_id = url.split("=")[1]
 
-    video_downloader(meeting_url, meeting_id, dest)
+    path = "output" + get_separator() + meeting_id
+    create_directory(path)
+
+    downloader(meeting_deskshare_url, path, "deskshare.webm", meeting_id)
+    downloader(meeting_webcam_url, path, "webcams.webm", meeting_id)
+
+    mp4_processor(path, meeting_id)
 
 if __name__ == '__main__':
     main()
-
