3577fc58990ead013349d4c5ca89ec0d21856d88
zathizh <sathish.bowatta@gmail.com>
2021-01-17 12:03:59 -0500

    add cleanup process

--- BBBDownlaoder.py
+++ BBBDownlaoder.py
@@ -46,21 +46,41 @@ def mp4_processor(path, _id):
     platform = get_platform()
 
     if platform == "linux":
-        fpath = path + get_separator()
         gen_audio_cmd = "ffmpeg -i " + fpath + "webcams.webm -vn -acodec copy " + fpath + "audio_out.opus"
         prep_mp4_cmd = "ffmpeg -i " + fpath + "deskshare.webm -i " + fpath + "audio_out.opus -map 0:v -map 1:a -c copy -y " + fpath + _id + ".webm"
         os.system(gen_audio_cmd)
         os.system(prep_mp4_cmd)
 
     elif platform == "win32" or platform == "windows" :
-        fpath = path + get_separator()
         gen_audio_cmd = ".\\bin\\ffmpeg.exe -i " + fpath + "webcams.webm -vn -acodec copy " + fpath + "audio_out.opus"
         prep_mp4_cmd = ".\\bin\\ffmpeg.exe -i " + fpath + "deskshare.webm -i " + fpath + "audio_out.opus -map 0:v -map 1:a -c copy -y " + fpath + _id + ".webm"
         os.system(gen_audio_cmd)
         os.system(prep_mp4_cmd)
 
     else :
-        exit("[-] Invalid Platform, Unable to convert files !!!")
+        print("[-] Invalid Platform, Unable to convert files !!!")
+        exit()
+
+def cleanup(path):
+    platform = get_platform()
+    fpath
+    if platform == "linux" or platform == "darwin":
+        rm_audio_cmd = "rm -rf " + path + "audio_out.opus"
+        rm_deskshare_cmd = "rm -rf " + path + "deskshare.webm"
+        rm_deskshare_cmd = "rm -rf " + path + "webcams.webm"
+        os.system(rm_audio_cmd)
+        os.system(rm_deskshare_cmd)
+        os.system(rm_deskshare_cmd)
+    elif platform == "win32" or platform == "windows" :
+        rm_audio_cmd = "del /f " + path + "audio_out.opus"
+        rm_deskshare_cmd = "del /f " + path + "deskshare.webm"
+        rm_deskshare_cmd = "del /f " + path + "webcams.webm"
+        os.system(rm_audio_cmd)
+        os.system(rm_deskshare_cmd)
+        os.system(rm_deskshare_cmd)
+    else : 
+        print("[-] Invalud Platform, Unable to delete files")
+        exit()
 
 
 # --- MAIN FUNCTION ---
@@ -75,13 +95,15 @@ def main():
     meeting_webcam_url = str(re.sub("playback\/[a-zA-Z0-9\.\/]*\?meetingId=", "presentation/", url)) + "/video/webcams.webm"
     meeting_id = url.split("=")[1]
 
-    path = "output" + get_separator() + meeting_id
+    path = "output" + get_separator() + meeting_id + get_separator()
     create_directory(path)
 
     downloader(meeting_deskshare_url, path, "deskshare.webm", meeting_id)
     downloader(meeting_webcam_url, path, "webcams.webm", meeting_id)
 
     mp4_processor(path, meeting_id)
+
+    cleanup(path)
 
 if __name__ == '__main__':
     main()
